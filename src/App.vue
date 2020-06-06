e75e1bce2b8f92e3bcb157e82bbaa486189070e7
Ishan Gogna <ishangogna10@gmail.com>
2020-06-06 15:25:32 +0530

    added showBlogs

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <add-blog></add-blog>
+  <show-blogs></show-blogs>
 </template>
 
 <script>
 import addBlog from './components/addBlog.vue'
+import showBlogs from './components/showBlogs.vue'
+
 export default {
   components:{
     'add-blog':addBlog,
+    'show-blogs': showBlogs
   },
   name: 'app',
   data () {
---
+++ src/components/showBlogs.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id = "show-blogs">
+        <h1>All Blog Articles</h1>
+        <div v-for="blog in blogs" :key='blog' class="single-blog">
+            <h2>{{blog.title}}</h2>
+            <article>{{blog.body}}</article>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            blogs : [],
+        }
+    },
+
+    created: function(){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+            console.log(data);
+            this.blogs = data.body.slice(0,10);
+
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+#show-blogs{
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+.single-blog{
+    margin: 20px 0;
+    background: #eee;
+    padding: 20px;
+    box-sizing: border-box;
+
+}
+
+</style>
