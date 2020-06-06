6f956b88506bb900e188d8a100eb7ac44e4d39b0
Ishan Gogna <ishangogna10@gmail.com>
2020-06-06 21:28:12 +0530

    added snippet/rainbow filter

--- src/main.js
+++ src/main.js
@@ -4,6 +4,22 @@ import VueResouse from 'vue-resource'
 
 Vue.use(VueResouse);
 
+//Custom Directives
+Vue.directive('rainbow',{
+  bind: function(el,binding,vnode){
+    el.style.color = "#" + Math.random().toString().slice(2,8);
+  }
+});
+
+//Custom Filters
+Vue.filter('to-uppercase',function(value){
+  return value.toUpperCase();
+})
+
+Vue.filter('snippet',function(value){
+  return value.slice(0,100) + '...';
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
