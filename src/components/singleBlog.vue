72dab2afcf5b3c5b83700aef7df6e6eed610a036
Ishan Gogna <ishangogna10@gmail.com>
2020-06-06 22:23:15 +0530

    added get Single blog from route param

---
+++ src/components/singleBlog.vue
@@ -0,0 +1,29 @@
+<template>
+ <div id = "single-blog">
+     <h1>{{blog.title}}</h1>
+     <article>{{blog.body }}</article>
+ </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            id: this.$route.params.id, //gets the 'id' in the router parameter
+            blog: {}
+        }
+    },
+
+    created: function(){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data){
+            console.log(data);
+            this.blog = data.body;
+        })
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
