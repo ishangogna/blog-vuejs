232f2f5a9c5fbf2e135d9b84e1a8252bc341c371
Ishan Gogna <ishangogna10@gmail.com>
2020-06-06 22:12:09 +0530

    added header component with router-link

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
+<div>
+  <app-header></app-header>
   <router-view></router-view>
+
+</div>
+  
 </template>
 
 <script>
 import addBlog from './components/addBlog.vue'
 import showBlogs from './components/showBlogs.vue'
+import appHeader from './components/header'
 
 export default {
   components:{
     'add-blog':addBlog,
-    'show-blogs': showBlogs
+    'show-blogs': showBlogs,
+    'app-header': appHeader,
   },
   name: 'app',
   data () {
---
+++ src/components/header.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav>
+        <ul>
+            <li><router-link to= "/" exact>Blogs</router-link></li>  <!-- exact makes the link active when the url -->
+            <li><router-link to= '/add' exact>Add a blog</router-link></li> <!-- is exactly / or /add, helping with router-link-active -->
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    nav{
+        background: #444;
+        padding: 5px;
+    }
+
+    nav ul {
+        list-style-type: none;
+        text-align: center;
+    }
+    nav li{
+        display: inline-block;
+        margin: 0 20px;
+
+    }
+    a{
+        text-decoration: none;
+        color: #fff;
+        border-radius: 10px;
+        padding: 6px 8px;
+    }
+    .router-link-active{
+        background: #eee;
+        color: darkgrey;
+    }
+
+</style>
